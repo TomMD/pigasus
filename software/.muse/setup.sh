a17b118bc961a182fbd460910f2730743a0570a1
Thomas M. DuBuisson <tommd@muse.dev>
2020-11-06 15:54:41 -0800

    Install deps

--- software/.muse/setup.sh
+++ software/.muse/setup.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/bash
-apt update && apt install -y libdaq-dev libdnet-dev hwloc libhwloc-dev libluajit-5.1-dev libpcap-dev
-git clone https://github.com/ofalk/libdnet
-cd libdnet
-./configure && make && make install
+
+$(git rev-parse --show-toplevel)/install_deps.sh
